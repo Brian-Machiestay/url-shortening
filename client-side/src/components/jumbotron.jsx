423fa26b082476926404b0b8f3aa583495a33d84
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-27 20:59:40 +0000

    create jumbotron component

---
+++ client-side/src/components/jumbotron.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Jumbotron = () => {
+    return (
+        <div className="jumbotron">
+            <div className="textJumbo">
+                <h2>More than just shorter links</h2>
+                <p className="brand">Build your brand recognition and get detailed insights on how your links are performing</p>
+                <a href="#" className="start">Get started</a>
+            </div>
+            <div className="imageJumbo">
+                <img src=""></img>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron;
