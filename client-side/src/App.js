a1b99712e7c71de7d1521181a7c27b634e2c5baf
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-27 16:50:49 +0000

    create the header component

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import './App.css';
+import HeaderComponent from './components/header';
+
 
 function App() {
   return (
-    <div className="App">
-      <p>HelloWorld</p>
-    </div>
+     <HeaderComponent />
   );
 }
 
---
+++ client-side/src/components/header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function headerComponent() {
+    return (
+    <div>
+        <ul>
+            <li>Features</li>
+            <li>Pricing</li>
+            <li>Resources</li>
+            <li>Login</li>
+            <li>Sign up</li>
+        </ul>
+    </div>
+    )
+}
+
+export default headerComponent;
