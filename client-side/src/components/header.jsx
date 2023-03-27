e627b6e0e4796a60d90b3cf90f60faa518847612
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-27 17:21:54 +0000

    create styles for header component

--- client-side/src/components/header.jsx
+++ client-side/src/components/header.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 
-function headerComponent() {
+function HeaderComponent() {
     return (
-    <div>
-        <ul>
+    <div className="header">
+        <ul className="nav">
+            <li className="logo">Shortly</li>
             <li>Features</li>
             <li>Pricing</li>
             <li>Resources</li>
-            <li>Login</li>
-            <li>Sign up</li>
+            <li className="login">Login</li>
+            <li className="signup">Sign up</li>
         </ul>
     </div>
     )
 }
 
-export default headerComponent;
+export default HeaderComponent;
