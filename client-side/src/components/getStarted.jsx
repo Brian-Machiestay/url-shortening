48400a5d9316fc7e57222e941909d5e76533dc3b
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-27 22:21:23 +0000

    create a get started component

---
+++ client-side/src/components/getStarted.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GetStarted = () => {
+    return <a href="#" className="start">Get Started</a>;
+}
+
+export default GetStarted;
--- client-side/src/components/jumbotron.jsx
+++ client-side/src/components/jumbotron.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import jumbologo from '../assets/images/illustration-working.svg';
+import GetStarted from "./getStarted";
 
 const Jumbotron = () => {
     return (
@@ -7,7 +8,7 @@ const Jumbotron = () => {
             <div className="textJumbo">
                 <h2>More than just shorter links</h2>
                 <p className="brand">Build your brand recognition and get detailed insights on how your links are performing</p>
-                <a href="#" className="start">Get started</a>
+                <GetStarted />
             </div>
             <div className="imageJumbo">
                 <img src={ jumbologo } alt="working illustration" />
