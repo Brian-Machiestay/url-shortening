d9e04114594f41b3ac15163cf6227b1b07c5f632
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-28 19:15:50 +0000

    connect brand statistics components

--- client-side/src/components/statisticSection.jsx
+++ client-side/src/components/statisticSection.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Shorten from "./shorten";
 import BrandStat from "./stat";
-import recogniton from "../assets/images/icon-brand-recognition.svg"
+import recogniton from "../assets/images/icon-brand-recognition.svg";
+import detailed from "../assets/images/icon-detailed-records.svg";
+import constom from "../assets/images/icon-fully-customizable.svg";
 
 const StatSection = () => {
     return (
@@ -14,7 +16,13 @@ const StatSection = () => {
             <p>Lorem ipsum kldfjkdlfd fdflkjdfkjdfjdfdjfk dfkdlfdfjldkfslfslfs
                 djfldkfjdlfjdfdfsjldkfslkf
             </p>
-            <BrandStat cls="recognize" icon={recogniton} />
+            <div className="iconItems">
+                <BrandStat cls="recognize" icon={recogniton} />
+                <div className="connect"></div>
+                <BrandStat cls="detailed" icon={detailed} />
+                <div className="connect"></div>
+                <BrandStat cls="custom" icon={constom} />
+            </div>
         </div>
     )
 }
