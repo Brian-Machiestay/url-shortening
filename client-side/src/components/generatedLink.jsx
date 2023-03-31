7c81415b6122fd82fbd1d93fcb23ca700be3e99b
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-31 15:43:00 +0000

    create generated link component

---
+++ client-side/src/components/generatedLink.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import GetStarted from "./getStarted";
+
+const GeneratedLink = (props) => {
+    return (
+        <div>
+            <p>your link</p>
+            <p>generated link</p>
+            <GetStarted />
+        </div>
+    )
+}
+
+export default GeneratedLink;
